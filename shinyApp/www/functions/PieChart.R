af454dd4fa40839524fec122db9b15556ad6b09a
Chenchen Jiang <jiang.chenc@northeastern.edu>
2023-04-03 20:54:11 -0700

    changed on pie chart function

--- shinyApp/www/functions/PieChart.R
+++ shinyApp/www/functions/PieChart.R
@@ -10,9 +10,10 @@ ggplot( data= prac2) +
 # Function of Pie Chart
 createPieChart <- function(df, colName) {
   ggplot(data = df) + 
-    geom_col(mapping = aes(x = "", y = df[[colName]], fill = Type)) +
+    geom_col(mapping = aes(x = "", y = .data[[colName]], fill = Type)) +
     coord_polar(theta = "y") +
     theme_bw() +
     labs(title = paste0("Pie Chart of ", colName), y = "Frequency")
 }
 
+createPieChart(prac2, "Freq")
