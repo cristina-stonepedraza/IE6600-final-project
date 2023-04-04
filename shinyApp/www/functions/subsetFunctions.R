1515dee1054e6b079b79d54cf935ec1d431f4b6e
Chenchen Jiang <jiang.chenc@northeastern.edu>
2023-04-03 19:36:24 -0700

    created pie chart and the function

--- shinyApp/www/functions/subsetFunctions.R
+++ shinyApp/www/functions/subsetFunctions.R
@@ -21,7 +21,6 @@ ggplot(prac2, aes(x = Type, y = Freq, fill = Type)) +
   theme(axis.text.x = element_text(angle = 45, vjust = 0.5))
 
 
-
 # Function - almost works (can't get line 30 to work properly, gives weird y-axis)
 
 chooseSub <- function(df, colName){
@@ -36,11 +35,35 @@ chooseSub <- function(df, colName){
 
 chooseSub(subsetEdu2, "Some college")
 
+##### Does the function work?
+chooseSub2 <- function(df, colName){
+  df_plot <- ggplot(data = df, mapping = aes_string(x = colName)) 
+  df_plot + 
+    geom_histogram(stat = "count") +
+    labs(title = "Frequency of Drinking Habits") +
+    theme(axis.line.x = element_text(angle = 45))
+}  
 
-
+chooseSub2(subsetEdu2, "X1")
 
 # Practice pie chart 
 #prac1 <- subsetEdu2 %>%
 #  select('Some college')
 
 #pie(table(prac1))
+
+# Create pie chart of "Some College" people frequency of drinking
+ggplot( data= prac2) + 
+  geom_col(mapping =  aes(x =  "", y = Freq, fill = Type)) +
+  coord_polar(theta = "y") +
+  theme_bw()+
+  labs(title = "Pie Chart of Drinking Frequency for Some College Education Status", y = "Frequency")
+
+# Function of Pie Chart
+createPieChart <- function(df, colName) {
+  ggplot(data = df) + 
+    geom_col(mapping = aes(x = "", y = df[[colName]], fill = Type)) +
+    coord_polar(theta = "y") +
+    theme_bw() +
+    labs(title = paste0("Pie Chart of ", colName), y = "Frequency")
+}
