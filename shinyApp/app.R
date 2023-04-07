7068e9677a29de366e4a5e606850630aeea94c5c
cristina-stonepedraza <cris24@pdx.edu>
2023-04-06 19:47:48 -0700

    skeleton for UI/dashboard

--- shinyApp/app.R
+++ shinyApp/app.R
@@ -1,23 +1,54 @@
-## app.R ##
+# app.R
+install.packages("shinydashboard")
+
 library(shiny)
 library(shinydashboard)
 
-ui <- dashboardPage(title = "Effects of Excessive Alcohol Use in the U.S.",
-  dashboardHeader(title = "Effects of Excessive Alcohol Use in the U.S."),
-  dashboardSidebar(),
+source("www/figures/gallonChartUS.R")
+#source("")
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Alcohol Use"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Home", tabName = "home"),
+      menuItem("US & Regional", tabName = "maps"), 
+      menuItem("Demographics", tabName = "demographics"), 
+      menuItem("Effects & Outcomes", tabName = "effects")
+    )
+  ),
   dashboardBody(
-    # Boxes need to be put in a row (or column)
-    fluidRow(
-      box(plotOutput("plot1", height = 250)),
+    tabItems(
+      # First tab content
+      tabItem(tabName = "home",
+              h2("intro to dataset and problem"),
+              fluidRow(
+                box(plotOutput("plot1", height = 250)),
+                
+                box(
+                  title = "Controls",
+                  sliderInput("slider", "Number of observations:", 1, 100, 50)
+                )
+              )
+      ),
       
-      box(
-        title = "Controls",
-        sliderInput("slider", "Number of observations:", 1, 100, 50)
+      # Second tab content
+      tabItem(tabName = "maps",
+              h2("use map and regional data")
+      ), 
+      
+      tabItem(tabName = "demographics",
+        h2("use subset data")
+      ), 
+      
+      tabItem(tabName = "effects", 
+        h2("use ARDI data")
       )
     )
   )
 )
-server <- function(input, output){ 
+
+server <- function(input, output) {
   set.seed(122)
   histdata <- rnorm(500)
   
--- shinyApp/ui.R
+++ shinyApp/ui.R
@@ -1,30 +1,8 @@
-## app.R ##
+## ui.R ##
 library(shinydashboard)
 
-ui <- dashboardPage(
-  dashboardHeader(title = "Basic dashboard"),
+dashboardPage(
+  dashboardHeader(),
   dashboardSidebar(),
-  dashboardBody(
-    # Boxes need to be put in a row (or column)
-    fluidRow(
-      box(plotOutput("plot1", height = 250)),
-      
-      box(
-        title = "Controls",
-        sliderInput("slider", "Number of observations:", 1, 100, 50)
-      )
-    )
-  )
+  dashboardBody()
 )
-
-server <- function(input, output) {
-  set.seed(122)
-  histdata <- rnorm(500)
-  
-  output$plot1 <- renderPlot({
-    data <- histdata[seq_len(input$slider)]
-    hist(data)
-  })
-}
-
-shinyApp(ui, server)
